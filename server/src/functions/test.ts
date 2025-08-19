653044388bdfccd2cc59736ec29c52eb0613ffea
Rupesh Shrestha <rupeshshrestha2537@gmail.com>
2025-08-20 00:37:49 +1000

    Testng typescript

---
+++ server/src/functions/test.ts
@@ -0,0 +1,13 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+
+export async function test(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    return { body: `Success` };
+};
+
+app.http('test', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: test
+});
