fe700799a15f5ef59795c6de9b683b8c34e831a9
Rupesh Shrestha <53545877+rupesh110@users.noreply.github.com>
2025-08-19 15:07:58 +1000

    Update index.js

--- server/src/functions/index.js
+++ server/src/functions/index.js
@@ -1,23 +1,4 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath, pathToFileURL } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-function loadFunctions(dir) {
-  fs.readdirSync(dir).forEach(file => {
-    const fullPath = path.join(dir, file);
-    const stat = fs.statSync(fullPath);
-
-    if (stat.isDirectory()) {
-      loadFunctions(fullPath);
-    } else if (file.endsWith('.js') && file !== 'index.js') {
-      // Convert Windows path to file:// URL
-      const fileUrl = pathToFileURL(fullPath).href;
-      import(fileUrl);
-    }
-  });
-}
-
-loadFunctions(__dirname);
+import "./Test.js"
+import "./addJob.js"
+import "./createUsers.js"
+import "./getAppliedJobs.js"
