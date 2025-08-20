7a622cf5e8b2414c93750c19cab5eee113abebbc
Rupesh Shrestha <rupeshshrestha2537@gmail.com>
2025-08-20 11:33:13 +1000

    connecting db Testing

--- server/src/functions/test.ts
+++ server/src/functions/test.ts
@@ -1,9 +1,19 @@
 import { app } from "@azure/functions";
-import {Testing} from "./Test1212/Testing";
+import { Testing } from "./Test1212/Testing";
+import { AddJob } from "./Jobs/addJob";
 
+// Test endpoint
 app.http('Test', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     route: "v2/test",
     handler: Testing
+});
+
+// Add Job endpoint
+app.http('AddJob', {
+    methods: ['POST'],
+    authLevel: 'anonymous',
+    route: "jobs/add",
+    handler: AddJob
 });
